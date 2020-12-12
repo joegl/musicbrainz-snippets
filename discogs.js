b7c0042f9b7788af13248fbabbba0210314985fe
Joe Gilliland-Lloyd <6943710+joegl@users.noreply.github.com>
2020-12-12 13:30:13 -0600

    Update discogs.js

--- discogs.js
+++ discogs.js
@@ -1,3 +1,5 @@
+// https://github.com/joegl/musicbrainz-snippets/
+
 // get artist and release (album title)
 var artist = document.getElementById("profile_title").getElementsByTagName('span')[0].getElementsByTagName('span')[0].getElementsByTagName('a')[0].innerHTML.trim();
 var release = document.getElementById("profile_title").getElementsByTagName('span')[2].innerHTML.trim();
@@ -12,8 +14,11 @@ for (var i = 0, row; row = tracklist_table.rows[i]; i++) {
   var track_title = row.getElementsByClassName('track')[0].getElementsByClassName('tracklist_track_title')[0].innerHTML;
   var track_time = row.getElementsByClassName('tracklist_track_duration')[0].getElementsByTagName('span')[0].innerHTML;
   var track = track_num + ". " + track_title;
-  track += " (" + track_time + ")\n"; 
+  if(track_time != '') {
+    track += " (" + track_time + ")"; 
+  }
+  
   //console.log(track);
-  tracklist += track;
+  tracklist += "\n" + track;
 }
 console.log(tracklist);
